d34716a48a560f2f59cc0543bb4a3559402239c1
turpp <white.tripp@icloud.com>
2020-11-09 16:03:55 -0600

    Done.

--- app.rb
+++ app.rb
@@ -4,5 +4,17 @@ class App < Sinatra::Base
 		erb :index
 	end
 
+	get '/hello' do 
+		erb :hello
+	end
 
+	get '/date' do 
+		@today=Time.now.strftime("The date is %A, %B %d, %Y")
+		erb :date
+	end
+
+	get '/goodbye' do 
+		@name= "Joe"
+		erb :goodbye
+	end
 end
